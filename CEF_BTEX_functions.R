db83b3b4f4a3c67d198a8cb51266b8bee2133b38
Pham <phillip.pham@hc-sc.gc.ca>
2024-06-12 15:21:13 -0400

    Added summary statistics and seasonal comparison

--- CEF_BTEX_functions.R
+++ CEF_BTEX_functions.R
@@ -3,3 +3,85 @@
 hello_world = function(){
   print('Hello_world!')
 }
+
+
+#' Generate Summary Statistics
+#'
+#' @param data, tibble/dataframe with columns c(mean, sd, N, n_BDL, %_BDL, min, p25, p50, p75, max)
+#' @return flextable object
+generate_summary_statistics = function(data, species_levels){
+  require(flextable)
+  require(dplyr)
+  
+  # set season and species to factor
+  # note min and max are some kind of special keywords... you will get a bug by naming your variables min or max
+  data = data %>% dplyr::mutate(stat = 'Statistic',
+                                species = factor(species, levels = species_levels),
+                                season = factor(season, levels = c('fall', 'winter')),
+                                min_val = as.numeric(min),
+                                max_val = as.numeric(max)) %>%
+    dplyr::rename(Season =season, Species = species)
+  
+  # use tabulate to set up aggregation
+  tab = data %>% 
+    flextable::tabulator(x = .,
+                         rows = c('Species', 'Season'),
+                         columns = 'stat',
+                         n = flextable::as_paragraph(N),
+                         `n BDL` = flextable::as_paragraph(n_BDL),
+                         `% BDL` = flextable::as_paragraph(`%_BDL`),
+                         `μ` = flextable::as_paragraph(mean),
+                         `σ` = flextable::as_paragraph(sd),
+                         min = flextable::as_paragraph(as.numeric(min_val)),
+                         Q1 = flextable::as_paragraph(p25),
+                         median = flextable::as_paragraph(p50),
+                         Q3 = flextable::as_paragraph(p75),
+                         max = flextable::as_paragraph(as.numeric(max_val)))
+  
+  # return as flextable object, ready to render
+  return(flextable::as_flextable(tab))
+}
+
+
+
+seasonal_comparison_plot = function(data, stat_res, sigma = 0.01, logbase = 10) {
+  require(ggpubr)
+  require(ggplot2)
+  require(rstatix)
+  
+  y_name = bquote(paste("Concentration", " (", n * g, "/", m ^ 3, ")"))
+  y_range = c(0,3)
+  y_breaks = c(0.1, 0.5, 1, 2, 3)
+  y_breaks_minor = c(seq(0.1, 5, 0.1))
+  
+  stat_res$y.position = asinh(stat_res$y.position / (2 * sigma)) / log(logbase)
+  
+  plot = data %>%
+    dplyr::filter(species %in% stat_res$species) %>%
+    dplyr::mutate(species = factor(species, levels = stat_res$species)) %>%
+    ggpubr::ggboxplot(data = .,
+                      x = 'species',
+                      y = 'conc',
+                      fill = 'season') +
+    ggplot2::scale_y_continuous(name = y_name,
+                                trans = scales::pseudo_log_trans(sigma = 0.01, base = 10),
+                                breaks = y_breaks,
+                                minor_breaks = y_breaks_minor, labels = y_breaks) +
+    ggplot2::scale_x_discrete(name = 'Species') +
+    ggplot2::scale_fill_manual(name = 'Season', values = c('skyblue3', 'tomato3'), labels = c('Fall', 'Winter')) +
+    ggpubr::stat_pvalue_manual(data = stat_res, tip.length = 0) +
+    ggplot2::theme(legend.position = 'right',
+                   panel.grid.major.y = element_line(colour = 'grey75'),
+                   panel.grid.minor.y = element_line(colour = 'grey95'),
+                   axis.text.x = element_text(
+                     angle = 45,
+                     vjust = 1,
+                     hjust = 1,
+                     size = 8
+                   ), 
+                   axis.text.x.bottom = element_text(size = 8),
+                   axis.text.y.left = element_text(size = 8),
+                   text = element_text(size = 8))
+  
+  return(plot)
+}
--- CEF_BTEX_main_analysis.R
+++ CEF_BTEX_main_analysis.R
@@ -4,10 +4,107 @@
 
 # Load libraries + source functions
 library(tidyverse)
+library(stringr)
+library(flextable)
+library(moments)
+library(rstatix)
+library(ggpubr)
+library(readxl)
+library(rstatix)
 
-
+source('CEF_BTEX_functions.R')
 
 
 # Loading Data ------------------------------------------------------------
 
+# load data & remove data where voc data is missing (can easily do this by filtering out any VOC species by na values)
+# Note - units assumed to be ug/m3
+voc = readxl::read_xlsx(path = 'data/EGROW_passive_for_analysis.xlsx', sheet = 2, col_names = T) %>%
+  dplyr::filter(!is.na(Dichloromethane))
 
+# list of btex species
+voc_species = c('Dichloromethane', 'Hexane', 'Chloroform', '__2_Dichloroethane', 'Benzene', 'Trichloroethylene',
+                'Toluene', 'Tetrachloroethylene', 'Ethylbenzene', '_m_p__Xylene', 'o_Xylene', 'Styrene', 'Cumene', 
+                'a_Pinene', '__1_2_2_Tetrchloroethane', 'n_Decane', '__3_5_Trimethylbenzene', '__2_4_Trimethylbenzene', 
+                'Pentachloroethane', 'd_Limonene', 'p_Cymene', '__3_Dichlorobenzene', '__4_Dichlorobenzene', 'Hexachloroethane',
+                '__2_4_Trichlorobenzene', 'Naphthalene')
+
+
+# convert data to long format - for calculations and plotting later
+voc_long = voc %>%
+  dplyr::select(season, tidyselect::all_of(voc_species)) %>%
+  tidyr::pivot_longer(cols = tidyselect::all_of(voc_species), 
+                      names_to = 'species', 
+                      values_to = 'conc')
+
+# examine data distribution - does not look normally distributed. Should use Dunn Test or other non parametric approaches
+# data is also BDL-skewed; dataset is set such that < BDL is set to BDL
+voc_long %>%
+  ggplot(aes(x = conc, y = after_stat(density))) +
+  geom_density() +
+  facet_wrap(~species) +
+  coord_cartesian(xlim = c(0, 0.5)) +
+  theme_bw()
+
+
+# Data Wrangling ----------------------------------------------------------
+
+# compute number of below detection limits
+voc_BDL = voc %>% dplyr::select(season, contains("BDL")) %>%
+  tidyr::pivot_longer(cols = all_of(contains("BDL")), names_to = 'species', values_to = 'flag') %>%
+  dplyr::mutate(species = stringr::str_remove(string = species, pattern = '_BDL')) %>%
+  dplyr::group_by(season, species) %>%
+  dplyr::summarise(N = n(),
+                   n_BDL = sum(flag %in% c('1', '2')),
+                   `%_BDL` = (n_BDL/N) * 100) %>%
+  dplyr::select(season, species, `%_BDL`, n_BDL)
+
+# compute summary statstics: N, mean, SD, min, p25, p50/median, p75, max from voc_long
+voc_summary_stats = voc_long %>%
+  dplyr::group_by(season, species) %>%
+  dplyr::summarise(N = n(),
+                   mean = mean(conc),
+                   sd = sd(conc),
+                   min = min(conc),
+                   p25 = quantile(conc, probs = 0.25),
+                   p50 = quantile(conc, probs = 0.5),
+                   p75 = quantile(conc, probs = 0.75),
+                   max = max(conc)) %>%
+  dplyr::left_join(., voc_BDL, by = c('species', 'season')) %>%
+  dplyr::arrange(species, season) %>%
+  dplyr::select(species, season, N, n_BDL, `%_BDL`, min, max, mean, sd, p25, p50, p75)
+
+# generate and modify table
+summary_stats_flextable = generate_summary_statistics(voc_summary_stats, voc_species)
+summary_stats_flextable = summary_stats_flextable %>%
+  flextable::width(x = ., j = 3, unit = 'mm', width = 1) %>%
+  flextable::width(x = ., j = c(1,2), unit = 'mm', width = c(40,17)) %>%
+  flextable::width(x = ., j = c(4,5,6,7,8,9,10, 11, 12, 13),
+                   width = c(10, 13, 13, 10, 10, 10, 10, 15, 10, 10), unit = 'mm') %>%
+  flextable::fontsize(x = ., size = 8, part = c('all'))
+
+# save table
+summary_stats_flextable %>% flextable::save_as_docx(., path = 'tables/summary_stats.docx')
+
+
+
+# Effect of season on BTEX species concentration --------------------------
+
+# note that only 13 species had values above detection limits
+# species where all measurements were below detection limit were automatically excluded from analysis (due to 0 difference in means)
+voc_season_dunn = voc_long %>%
+  dplyr::group_by(species) %>%
+  rstatix::dunn_test(data = ., formula = conc ~ season, detailed = TRUE) %>%
+  rstatix::add_xy_position() %>%
+  dplyr::select(-groups)
+
+# some adjustments for plotting stats comparisons
+voc_season_dunn = voc_season_dunn %>%
+  dplyr::mutate(x = seq(1:nrow(.)),
+                xmin = x - 0.4,
+                xmax = x + 0.4,
+                group1 = 'fall',
+                group2 = 'winter')
+
+# we will only look at BTEX species where concentrations were above detection limit (list from voc_season_dunn)
+seasonal_comparison_plot(data = voc_long, stat_res = voc_season_dunn)
